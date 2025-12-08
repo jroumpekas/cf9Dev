f9507b5659d4ee4ccd43c202f92ff6aa73393025
Dim.Roumpekas <jimroumpi@gmail.com>
2025-12-09 01:09:51 +0200

    Upload ch12 files

---
+++ src/gr/aueb/cf/cf9/ch12/Main.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.cf9.ch12;
+
+public class Main {
+    public static void main(String[] args) {
+        Account aliceAccount = new Account(1L, "GR12345", "Alice", "W", "A12345",100.0);
+
+        try {
+            aliceAccount.deposit(500);
+            System.out.println(aliceAccount.accountToString());
+
+            aliceAccount.withdraw(100, "A12345");
+            System.out.println(aliceAccount.accountToString());
+
+            aliceAccount.withdraw(100, "A43232323");
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+
+        }
+    }
+}
