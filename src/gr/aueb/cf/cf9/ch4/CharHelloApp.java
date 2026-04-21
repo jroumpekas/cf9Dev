eee697d9dc0406226452862f0175c6c4a15bcba4
Dim.Roumpekas <jimroumpi@gmail.com>
2026-04-21 20:25:41 +0300

    Upload ch4 files

---
+++ src/gr/aueb/cf/cf9/ch4/CharHelloApp.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.cf9.ch4;
+
+public class CharHelloApp {
+    public static void main(String[] args) {
+        char h = 'H';
+        char e = 'e';
+        char l = 'l';
+        char o = 'o';
+        char exclMark = '!';
+
+        System.out.println(h);
+        System.out.println(e);
+        System.out.println(l);
+        System.out.println(l);
+        System.out.println(o);
+        System.out.println(exclMark);
+    }
+}
